9635f1ee60817d45976053a2efa10a4d730e61b2
dannyd4315 <dannyd4315@gmail.com>
2016-02-22 15:23:41 +1100

    Passed initial tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  enum role: [:user, :vip, :admin]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+    
+  after_initialize :set_default_role
+
+  private
+    def set_default_role
+      self.role ||= :user
+    end
 end
