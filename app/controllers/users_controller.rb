aa0f96ff5f67ced422009857f85f37ab1aa9ef3c
pajamaw <pjpeterww@gmail.com>
2016-03-12 00:52:57 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController <ApplicationController
+  def index
+    @user = current_user
+  end
+
+  def create
+    user = User.create(params[:user])
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    unless @user == current_user || current_user.try(:admin?) 
+    redirect_to root_path, notice: "Access denied."
+  end
+end
+
+ 
+end
