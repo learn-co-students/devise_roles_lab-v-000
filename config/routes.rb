ec63e096a758fd9e3170e755e7ee647e5fcbeb42
marcotsui2003 <marcotsui2003@yahoo.com>
2016-03-21 17:11:39 -0400

    for Stephen

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+  
   devise_for :users
   resources :users
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
