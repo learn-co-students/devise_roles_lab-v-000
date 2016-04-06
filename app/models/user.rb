1a80a24da237d4efe3dff5570116e90b70b70ffa
Brian <useyourbrian@gmail.com>
2016-04-06 06:34:01 -0400

    started lab

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+enum role:[:user, :vip, :admin]
+
 end
