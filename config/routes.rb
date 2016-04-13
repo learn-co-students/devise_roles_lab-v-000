2d44a0046a924530877b3db0ff1da3b07591ed47
shinyrachel <rachel.kory@gmail.com>
2016-04-13 18:44:13 -0400

    visitors feature spec passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+  get 'pages/about' => 'visitors#about'
   devise_for :users
-  resources :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
