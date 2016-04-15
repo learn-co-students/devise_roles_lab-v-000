43595c135a7052accc6d0db18fea612927f2af9c
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-04-15 11:21:17 -0400

    Done.

--- spec/features/users/user_delete_spec.rb
+++ spec/features/users/user_delete_spec.rb
@@ -2,6 +2,8 @@ include Warden::Test::Helpers
 Warden.test_mode!
 
 #Too slow and pending, so I deleted it. 
+#This was a hard lab. Be sure to watch https://www.youtube.com/watch?v=RTXyPhRBQ_4
+#and Avi will walk you through it step by step. =)
 
 
 
