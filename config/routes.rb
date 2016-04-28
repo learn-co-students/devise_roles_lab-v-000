62ab78730135767f8add0ed65085d29b6b733a74
Tom Johns <tommyskies@hotmail.com>
2016-04-28 17:38:21 -0600

    up to sign in spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :posts
+  resources :posts
+	devise_for :users
 
   root to: 'users#index'
-
+  
   resources :users
+  resources :posts
 end
