0b1005d072e3bcb612abdd829a530f2e45a9f23d
e-baker <eric@ericbaker.me>
2016-05-01 19:27:17 -0400

    add flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
   before_action :set_post, except: [:index, :new, :create]
 
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
 
   private
 
     def set_post
       @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:content, :user_id)
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,33 @@
 class UsersController < ApplicationController
   #authentication filter
-  before_action :authenticate_user, except: [:create, :new]
+  before_action :authenticate_user, except: [:index, :create, :new]
   #authorization filter
   before_action :authorize_user, except: [:create, :index, :new]
   # only admin can update or destroy users
   before_action :admin_user, only: [:update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.find_or_create_by(email: params[:email])
+    @user.update(password: params[:password])
+
+    redirect_to user_path(@user.id)
+  end
+
+  def edit
+    unless current_user.admin? || @user == current_user
+      redirect_to users_path
+    end
+  end
+
+  def update
+  end
 
   def destroy
     @user.destroy
