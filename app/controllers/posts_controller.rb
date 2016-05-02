a68fe8ff7b127d1d53ba2ef817e0f0494fb59406
e-baker <eric@ericbaker.me>
2016-05-01 21:26:19 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,32 @@
 class PostsController < ApplicationController
   before_action :set_post, except: [:index, :new, :create]
 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    redirect_to post_path(@post.id)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
   def update
     @post.update(post_params)
-    redirect_to post_path(@post)
+    redirect_to post_path(@post.id)
+  end
+
+  def destroy
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'users#index'
-  resources :users
+  resources :users do 
+    resources :posts
+  end
   resources :posts
 end
