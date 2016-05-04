7a95f4124e02d8a4ba6ea19fe0e174ef29a408fc
evanscloud <dijkstrascloud@gmail.com>
2016-05-04 13:21:40 -0400

    Use devise to create role-based authorization

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:create, :new]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if !current_user.admin? || !(current_user = @user)
+      flash[:alert] = "Access denied."
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    if current_user.admin?
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to root_path, alert: "User has been deleted."
+    else
+      redirect_to root_path, alert: "Access denied."
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:role)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
-
   root to: 'users#index'
-
   resources :users
 end
