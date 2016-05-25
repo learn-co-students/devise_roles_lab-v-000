b840956486ddf95ef7a9104bd4c366f7dff8ebde
mhnguyen289 <mhnguyen289@gmail.com>
2016-05-25 15:28:47 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
+# Rails.application.routes.draw do
+#   devise_for :users
+
+#   root to: 'users#index'
+#   resources :posts
+#   resources :users
+# end
+
+
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
 
   root to: 'users#index'
