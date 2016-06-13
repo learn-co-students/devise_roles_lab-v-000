5573e017a506c0dfe5e9792229e007edfed196af
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-13 14:27:36 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   devise_for :users
 
   root to: 'users#index'
