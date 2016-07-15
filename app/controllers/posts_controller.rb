3bb421cfe168cbfd5c0641b8e0e3c8eb244a921c
breiss <brad72287@gmail.com>
2016-07-15 18:11:30 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  def update
+    Post.find(params[:id]).update(post_params)
+    redirect_to "/"
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
