f5e26592c522958d830785f3e7fec26b7f0643e5
reichardn <reichardn@gmail.com>
2016-08-01 18:57:49 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,14 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
     @post.user = current_user
     @post.save
     redirect_to root_path
   end
 
   def update
+    @post = Post.find_by(params[:id])
     @post.update_attributes(post_params)
     redirect_to root_path 
   end
