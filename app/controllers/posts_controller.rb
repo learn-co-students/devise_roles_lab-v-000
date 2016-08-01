f0cd65006226f78a13d8c38e231ac7c8dbbcd1e2
reichardn <reichardn@gmail.com>
2016-08-01 18:43:57 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  load_and_authorize_resource
+  before_action :authorize_user
 
   def index
   end
@@ -19,6 +19,15 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+   
+
+
+  def authorize_user
+      unless current_user == @post.user || current_user.vip? || current_user.admin?
+        redirect_to post_path(id: @post.id), :alert => "Access denied."
+      end
   end
 
 end
