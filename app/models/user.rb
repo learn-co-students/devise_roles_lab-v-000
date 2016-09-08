bf24d7c351baf7d13b84b020f7e894d36d6f9496
Jesse Novotny <jessenovotny@gmail.com>
2016-09-08 16:28:20 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
+  has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum role: [:user, :vip, :admin]
+  after_initialize :default_role, if: :new_record?
+
+  def default_role
+    self.role ||= :user
+  end
 end
---
+++ db/migrate/20160908181635_add_content_to_post.rb
@@ -0,0 +1,5 @@
+class AddContentToPost < ActiveRecord::Migration
+  def change
+    add_column :posts, :content, :string
+  end
+end
