394ee8e42bcb9cd322dd05733325c9c58479338b
tpstar <hanlee12@gmail.com>
2016-09-18 11:22:08 -0500

    finish users controller spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user.try(:admin?)
+      @user.destroy
+      redirect_to users_path
+    else
+      redirect_to users_path
+    end
+  end
+
+  # private
+  # def song_params
+  #   params.require(:song).permit(
+  #     :title, :release_year, :released, :genre, :artist_name
+  #   )
+  # end
 end
