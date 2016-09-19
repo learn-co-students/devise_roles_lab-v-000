d9c6299798e78b59dd8b61aa87c39733114a2bff
peterberwind <peterberwind@gmail.com>
2016-09-19 15:39:22 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :find_user, except: [:create, :index, :new]
+  before_action :find_user, except: [:create, :index, :new, :show]
 
   def index
     @users = User.all
   end
 
   def show
-    if current_user.id != @user.id
-      redirect_to root_path, :flash => { :error => "Access denied." }
+    unless current_user.admin?
+      unless @user == current_user
+        redirect_to root_path, :alert => "Access denied."
+      end
     end
   end
 
