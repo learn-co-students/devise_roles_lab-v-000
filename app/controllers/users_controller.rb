3fd809bbe71a85bdd827249a9e8cddf6a05d9318
Traci7822 <Tracir@gmail.com>
2016-10-19 13:59:28 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
 
+  def home
+    redirect_to root_path
+  end
+
   def index
+    @users = User.all
+  end
+
+  def create
   end
 
   def show
+    binding.pry
+    @user = User.find(params[:id])
+    if current_user.id == @user.id
+      redirect_to "/users/'#{current_user.id}'"
+    end
   end
 
   def destroy
