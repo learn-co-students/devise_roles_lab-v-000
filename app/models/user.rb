376a650f37a0fe5268a225839d35f8d6107eb9ad
Rebecca <beckysherman000@gmail.com>
2016-10-31 15:46:04 -0400

    Pass all features/users specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable
+  
   enum role: [:user, :vip, :admin]
 
   after_initialize :set_default_role
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'users#index'
+  root 'users#index'
 
   resources :users
+  resources :visitors
 end
