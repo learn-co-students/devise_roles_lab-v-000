52049aed63dddd701a6fa67519d76a66540e2f73
rgylling <mikegylling@yahoo.com>
2016-12-07 10:35:32 -0800

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum role: [:user, :vip, :admin]
+
+  after_initialize :set_default
+  
+  def set_default
+    self.role ||= :user
+  end
 end
