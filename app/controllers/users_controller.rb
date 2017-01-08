e3cbda07afede3789105dc5058913d4174a53508
andresduarte <finnleyam@gmail.com>
2017-01-08 00:28:17 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+
+class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :new, :create]
+  before_action :admin_only, only: [:destroy]
+
+  def index
+    @users = User.all
+    #@user = current_user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if !current_user.admin? && current_user != @user
+      flash[:error] = "Access denied."
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    return head(:forbidden) unless current_user.admin?
+    @user.destroy
+    redirect_to root_path, notice: "User has been deleted."
+  end
+
+  private
+
+    def admin_only
+      unless current_user.admin? || current_user == @user
+        redirect_to users_path, alert: "Access denied."
+      end
+    end
+
+end
