4d07abc1a76a6748e5db99214bda5d941a24b15e
neener <janinemrosen@gmail.com>
2017-01-22 02:51:01 -0500

    done

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :find_post!, only: [:edit, :show, :update]
+	before_action :authorize_user!, only: [:edit, :update]
+
+	def index
+	end
+	
+	def create
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def update
+		@post.update!(content: params[:post][:content])
+		redirect_to post_path, id: @post.id	
+	end
+
+	def destroy
+		return head(:forbidden) unless current_user.admin? || current_user.vip? || current_user.try(:id) == @post.user.id
+		@post.destroy
+	end
+
+	private
+
+	def authorize_user!
+		raise "Unauthorized!" unless current_user
+
+		unless current_user == @post.user || current_user.vip? || current_user.admin?
+			redirect_to post_path(id: @post.id), :alert => "Access denied."
+		end
+	end
+
+	def post_params
+		params.require(:post).permit(:user_id, :content)
+	end
+
+	def find_post!
+		@post = Post.find(params[:id])
+	end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+		if !current_user.admin? && current_user != @user
+			flash[:alert] = "Access denied."
+			redirect_to root_path
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		return head(:forbidden) unless current_user.admin?
+		@user.destroy
+		redirect_to root_path, notice: "User has been deleted."
+	end
+end
