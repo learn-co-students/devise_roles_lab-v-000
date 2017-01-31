99347124a5a127459e2c3de293ac33eea29da0ad
NATHAN ROBBERT <admin@bigrocketgames.com>
2017-01-31 06:30:45 +0000

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_post!, only: [:edit, :show, :update]
+  before_action :authorize_user!, only: [:edit, :update]
+
+  def new
+  end
+
+  def create
+    user_id = current_user.id
+    Post.create(content: params[:post][:content], user_id: user_id)
+    redirect_to user_path(id: user_id)
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update!(content: params[:post][:content])
+    redirect_to post_path(@post)
+  end
+
+
+  private
+
+  def authorize_user!
+    raise "Unauthorized!" unless current_user
+
+    unless current_user == @post.user || current_user.vip? || current_user.admin?
+      redirect_to post_path(@post), :alert => "Access denied."
+    end
+  end
+
+  def find_post!
+    @post = Post.find(params[:id])
+  end
+
+end
