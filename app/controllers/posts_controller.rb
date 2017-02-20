f1e5b66984734b8db85feed43863e7e2badca482
Scott Young <young.scottw@gmail.com>
2017-02-20 15:09:17 -0600

    Ok now they are working for sure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,13 @@
 class PostsController < ApplicationController
-  before_action :admin_or_vip_only, only: [:edit, :update]
+  before_action :authenticate_user!
 
   def update
     @post = Post.find(params[:id])
+
+    unless current_user = @post.user || current_user.admin? || current_user.vip?
+      redirect_to post_path(id: @post.id), :alert => "Access denied."
+    end
+    
     @post.update!(post_params)
     redirect_to post_path, id: @post.id
   end
@@ -13,11 +18,4 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content)
   end
 
-  def admin_or_vip_only
-    raise "Unauthorized!" unless current_user
-
-    unless current_user.admin? || current_user.vip?
-      redirect_to posts_path, :alert => "Access denied."
-    end
-  end
 end
