df439c539283d8ccde9d2531491f64c828091a12
Scott Young <young.scottw@gmail.com>
2017-02-20 15:11:07 -0600

    This lab sucks, I think posts controller is working right now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,10 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    unless current_user = @post.user || current_user.admin? || current_user.vip?
-      redirect_to post_path(id: @post.id), :alert => "Access denied."
+    unless current_user == @post.user || current_user.admin? || current_user.vip?
+      return redirect_to post_path(id: @post.id), :alert => "Access denied."
     end
-    
+
     @post.update!(post_params)
     redirect_to post_path, id: @post.id
   end
