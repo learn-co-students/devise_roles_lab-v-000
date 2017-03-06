f7706a30860c462a54c1f7a34de05c3fc827bdd7
David Kennell <davidkennell0@gmail.com>
2017-03-06 09:13:22 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
 
   def index
+  	@users = User.all
   end
 
-  
+  def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to :back, :alert => "Access denied."
+    end
+  end
+
+  def destroy
+	u = User.find_by(id: params[:id])
+	# Problem here is that we should check
+	# for current user instead of the user
+	# we're changing.
+  	if current_user.role == "admin" || u.role == "vip"
+  		u.delete
+  	end
+    redirect_to root_path
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118210027) do
+ActiveRecord::Schema.define(version: 20170302190320) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "content"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,7 +34,7 @@ ActiveRecord::Schema.define(version: 20160118210027) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "role"
+    t.integer  "role",                   default: 0
     t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
