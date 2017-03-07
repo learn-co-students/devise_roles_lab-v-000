df847ff8a41d040354aad1df8e9c763982cf4742
Gordon <c9.list@gsoyd.com>
2017-03-07 00:43:27 +0000

    Done.

--- db/migrate/20170306192308_create_posts.rb
+++ db/migrate/20170306192308_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.integer :owner_id
+      t.integer :user_id
       t.string :content
 
       t.timestamps null: false
