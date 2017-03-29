b1e71ef5ed0a9510bdd94fdd571b8be0e2d320f3
Kevin Adkins <kevinladkins@gmail.com>
2017-03-29 20:04:35 +0000

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+  before_action :set_post!, except: [:index, :new, :create]
+
+ def index
+   @posts = Post.all
+ end
+
+ def new
+   @post = Post.new
+ end
+
+ def create
+   @post = Post.create(post_params)
+   redirect_to @post
+ end
+
+ def edit
+   if current_user.user?
+     redirect_to root_path unless current_user.id == @post.user_id
+   end
+ end
+
+ def update
+   if current_user.user?
+     redirect_to root_path unless current_user.id == @post.user_id
+   else
+     @post.update(post_params)
+     redirect_to @post
+   end
+ end
+
+ def destroy
+   if current_user.user?
+     redirect_to root_path
+   else
+     @post.destroy
+     redirect_to root_path
+   end
+ end
+
+
+
+
+
+ private
+
+ def post_params
+   params.require(:post).permit(:content, :owner_id)
+ end
+
+ def set_post!
+   @post = Post.find(params[:id])
+ end
+end
