125df4bf9ce443df029824cfc354225281464540
Alex Glover <Alex@MacBook-Pro-3.local>
2017-04-10 10:13:15 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :logged_in?
+  helper_method :current_user
+
+  def logged_in?
+    !!current_user
+  end
+
+  def ensure_logged_in
+    redirect_to root_path, alert: "Access denied." unless logged_in?
+  end
+
+  def ensure_vip
+    unless current_user.vip? || current_user.admin?
+      redirect_to root_path, alert: "Access denied."
+    end
+  end
+
+  def ensure_admin
+    redirect_to root_path, alert: "Access denied." unless current_user.admin?
+  end
 end
