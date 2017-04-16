501280b3fe3e1e721fa21fdc53bc3c50d4e4a269
John Pfingst <jpfingst@outlook.com>
2017-04-16 05:02:44 +0000

    all tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
   has_many :posts
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum role: [:user, :vip, :admin]
+
+  after_initialize :set_default_role
+
+private
+
+  def set_default_role
+    self.role = :user
+  end
+
 end
