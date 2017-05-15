b0a6218510bb19e7146fe0ef261fea639671d72a
Howard DeVennish <howarddevennish@gmail.com>
2017-05-15 19:34:48 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
   def destroy
+    @user = User.find_by(id: params[:id])
+    if current_user.admin?
+      @user.destroy
+    end
     redirect_to root_path
   end
 end
