5f0807f0ca27e329060c545b89eb74f215a8e209
Howard DeVennish <howarddevennish@gmail.com>
2017-05-15 22:23:56 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+  before_action find_post, only: [:create, :update, :show]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    redirect_to post_show_path
+  end
+
+  def update
+    @post = Post.find_by(id: post_params[:id])
+    @post.update(post_params)
+    redirect_to post_show_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def find_post
+    @post = Post.find_by(id: params[:id])
+  end
 end
