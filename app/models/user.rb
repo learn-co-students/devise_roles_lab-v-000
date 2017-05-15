74f483bd8916b56c54422c9b3f267bdb9ad99659
Howard DeVennish <howarddevennish@gmail.com>
2017-05-15 17:24:23 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum role: [:user, :vip, :admin]
+
+  after_initialize :assign_default_role#, if: :new_record?
+
+  def assign_default_role
+    self.role ||= :user
+  end
 end
