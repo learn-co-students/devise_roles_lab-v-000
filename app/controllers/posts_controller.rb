cb437a21dadf23e70b0c49039b4edc3f04f77500
umohm1 <umohm12@gmail.com>
2017-05-25 10:56:17 -0400

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+    def index
+      @posts = Post.all
+    end
+
+    def create
+     user_id = current_user.id
+
+     Post.create(
+      content: params[:post][:content],
+      user_id: user_id
+     )
+
+     redirect_to user_path(id: user_id)
+    end
+
+
+    def update
+        @post = Post.find(params[:id])
+        return head(:forbidden) unless current_user.admin? ||
+                                       current_user.vip?   ||
+                                       current_user.try(:id) == @post.id
+        @post.update(post_params)
+        redirect_to post_path(@post)
+      end
+
+
+    private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
+    def find_post
+    @post = Post.find(params[:id])
+  end
+end
