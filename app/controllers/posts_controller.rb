67628a901ec203eec2c3c0324a927a31a3e3cf41
Andrew <ajoeford@gmail.com>
2017-06-27 12:58:32 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,17 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def edit
