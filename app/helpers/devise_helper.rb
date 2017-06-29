b9d39b5da371edecc937b1413480a9870902f26f
nadinesk <nadine.khattak@gmail.com>
2017-06-29 18:15:45 -0400

    Done.

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,9 @@
+module DeviseHelper
+
+ def devise_error_messages!
+    if resource.errors.full_messages.any?
+        flash.now[:error] = resource.errors.full_messages.join(' & ')
+    end
+    return ''
+  end
+end
