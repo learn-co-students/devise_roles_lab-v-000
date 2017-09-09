f92d78904db27537b6fe3589926bcd8ad861629c
Scott Atkins <satkins16@gmail.com>
2017-09-09 23:24:34 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:create, :new]
+  before_action :set_user!, except: [:create, :index, :new]
+  before_action :admin_only, only: :destroy
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, :notice => "User deleted."
+  end
+
+  private
+
+  def admin_only
+    unless current_user.admin? || @user == current_user
+      redirect_to users_path, :alert => "Access denied."
+    end
+  end
+
+  def secure_params
+    params.require(:user).permit(:role)
+  end
+
+  def set_user!
+    @user = User.find(params[:id])
+  end
+
+end
--- spec/support/helpers.rb
+++
@@ -1,9 +0,0 @@
-require 'support/helpers/controller_helpers'
-require 'support/helpers/session_helpers'
-
-RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-  config.include ControllerHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :controller
-  config.include Features::SessionHelpers, type: :feature
-end
