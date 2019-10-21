b6ec895aada9223dd224c832e0cb1c0a44f4488b
Steven G. Harms <steven.harms@flatironschool.com>
2019-10-21 09:24:29 -0400

    Nuke old Rails

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-
-  root to: 'users#index'
-
-  resources :users
-end
--- spec/factories/posts.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :post do
-    content "content"
-    association :user
-  end
-end
