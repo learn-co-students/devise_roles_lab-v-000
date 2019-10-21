7706acf6d7e533a85338fc83325d0955889b3ece
Steven G. Harms <steven.harms@flatironschool.com>
2019-10-21 09:50:25 -0400

    Restore User model test
    Use the role setting mechanism as suggested by:
    
    https://github.com/plataformatec/devise/wiki/How-To:-Add-a-default-role-to-a-User
    
    Adjust the create hook to use `.create` versus
    `.new` since we want to test (and probably ought
    to have been testing) this in an AR lifecycle context.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
+  after_validation :set_default_role
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum role: [:user, :vip, :admin]
+
+  private
+
+  def set_default_role
+    # Set default role here.
+  end
 end
